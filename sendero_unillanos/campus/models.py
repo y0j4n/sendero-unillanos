2c9a17d39058548da2661006d86c9e82a688bf8d
Yojan Styven <yojan.hernandez@unillanos.edu.co>
2020-11-20 11:48:46 -0500

    cambios en el index

--- sendero_unillanos/campus/models.py
+++ sendero_unillanos/campus/models.py
@@ -7,11 +7,15 @@ from singleton_model import SingletonModel
 class Campus(SingletonModel):
     titulo = models.CharField(max_length=100)
     banner = models.ImageField()
-    titulo_seccion1 = models.CharField(max_length=250)
-    parrafo_seccion1 = models.CharField(max_length=250)
-    img_seccion1 = models.ImageField(blank=True, null=True)
-    titulo_seccion2 = models.CharField(max_length=250, blank=True, null=True)
-    parrafo_seccion2 = models.CharField(max_length=250, blank=True, null=True)
+    subtitulo_seccion1 = models.CharField(max_length=250)
+    parrafo1_seccion1 = models.CharField(max_length=250, null=True, blank=True)
+    subtitulo_seccion2 = models.CharField(max_length=250)
+    parrafo1_seccion2 = models.CharField(max_length=250, null=True, blank=True)
+    parrafo2_seccion2 = models.CharField(max_length=250, null=True, blank=True)
+    parrafo3_seccion2 = models.CharField(max_length=250, null=True, blank=True)
+    subtitulo_seccion3 = models.CharField(max_length=250)
+    parrafo1_seccion3 = models.CharField(max_length=250, null=True, blank=True)
+    parrafo2_seccion3 = models.CharField(max_length=250, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Campus'
--- sendero_unillanos/presentacion/models.py
+++ sendero_unillanos/presentacion/models.py
@@ -14,15 +14,15 @@ class Presentacion(SingletonModel):
     titulo_de_la_pagina = models.CharField(max_length=50)
     icono_de_la_pagina = models.ImageField(blank=True, null=True)
     logo_de_la_pagina = models.ImageField(blank=True, null=True)
-    titulo_seccion1 = models.CharField(max_length=500)
-    parrafo_seccion1 = models.CharField(max_length=500)
-    img_seccion1 = models.ImageField(blank=True, null=True)
-    titulo_seccion2 = models.CharField(max_length=500)
-    parrafo_seccion2 = models.CharField(max_length=500)
-    img_seccion2 = models.ImageField(blank=True, null=True)
     banner = models.ImageField(blank=True, null=True)
-    mapa = models.ImageField(blank=True, null=True)
-
+    titulo_seccion1 = models.CharField(max_length=500)
+    img_seccion1 = models.ImageField(blank=True, null=True)
+    parrafo1_seccion1 = models.CharField(max_length=2000)
+    parrafo2_seccion1 = models.CharField(max_length=2000)
+    titulo_seccion2 = models.CharField(max_length=500)
+    img_seccion2 = models.ImageField(blank=True, null=True)
+    parrafo1_seccion2 = models.CharField(max_length=2000)
+    parrafo2_seccion2 = models.CharField(max_length=2000, blank=True, null=True)
     class Meta:
         verbose_name = 'Presentacion'
         verbose_name_plural = 'Presentacion'
